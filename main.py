5d5535b52ec5163c474470b5b2ffe9d1593b1908
Rainer Toots <rainer.toots.era@gmail.com>
2022-10-15 18:07:08 +0300

    Added comments and cleaned code a bit

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from tkinter import *
 import math
 import json
-import ProfileFrame
+from ProfileFrame import ProfileFrame
 
+# Start a Tkinter window
 window = Tk()
+
+# Set a good starting size
 window.geometry("800x500")
+
+# Title it
 window.title("Rolemaster Tracker")
+
+# Add padding
 window.config(padx=10, pady=10)
 
-# Initialize a frame
+# Initialize a frame - 1 frame -  1 profiel
 frame = Frame(window)
-frame.grid(row=1)
+frame.grid(row=1) # Position it
 
-BlankProfile = ProfileFrame.ProfileFrame(frame)
+# Call a profile to a certain frame
+BlankProfile = ProfileFrame(frame)
 
 if __name__ == "__main__":
     window.mainloop()
